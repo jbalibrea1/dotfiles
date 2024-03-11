c84ed75d0699940919ef3604a17d94b563d39749
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-03-12 00:25:15 +0100

    Added nvim-kickstart

---
+++ .config/nvim-kickstart/lua/custom/plugins/init.lua
@@ -0,0 +1,13 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+}
